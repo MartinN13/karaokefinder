d576c6b80246c1fe1ee1f7621f3c7895518ec6b9
MartinN13 <martinnabhan@me.com>
2016-11-24 23:40:20 +0900

    Add comment to change_column migration

--- db/migrate/20161123102526_change.rb
+++ db/migrate/20161123102526_change.rb
@@ -1,5 +1,6 @@
 class Change < ActiveRecord::Migration[5.0]
   def change
+    # Change the column types from date to string for easier validation tests
     change_column :karaokes, :day, :string
     change_column :karaokes, :night, :string
   end
